cb33794c3b0c31df9c2211befe95aaf374e98880
Dmitrii Zolotov <dmitrii.zolotov@gmail.com>
2014-08-30 00:46:44 +0400

    em->endpointManager

--- dispatcher/src/main/java/org/gathe/integration/Main.java
+++ dispatcher/src/main/java/org/gathe/integration/Main.java
@@ -22,6 +22,6 @@ public class Main {
     private static EndpointManager endpointManager = new EndpointManager();
 
     public static void main(String[] args) throws Exception {
-        em.start();
+        endpointManager.start();
     }
 }
